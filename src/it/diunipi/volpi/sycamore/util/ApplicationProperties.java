f2e3cc2eedd9dc38046d388946b567fb4c4e5b21
valecapannoli <valecapannoli@gmail.com>
2013-08-20 18:43:25 +0000

    mod: update code

--- src/it/diunipi/volpi/sycamore/util/ApplicationProperties.java
+++ src/it/diunipi/volpi/sycamore/util/ApplicationProperties.java
@@ -9,32 +9,42 @@ package it.diunipi.volpi.sycamore.util;
  */
 public enum ApplicationProperties implements SycamoreProperty
 {
+	WINDOW_X("X position of the app window", 0 + ""),
+	WINDOW_Y("Y position of the app window", 0 + ""),
+	WINDOW_WIDTH("Width of the app window", 1024 + ""),
+	WINDOW_HEIGHT("Height of the app window", 600 + ""),
+	GRID_VISIBLE("Grid in 3D scene is visible", true + ""),
+	AXES_VISIBLE("Grid in 3D scene is visible", true + ""),
+	BARICENTRUM_VISIBLE("Grid in 3D scene is visible", false + ""),
+	ROBOTS_LIGHTS_VISIBLE("Grid in 3D scene is visible", true + ""),
+	VISIBILITY_RANGES_VISIBLE("Grid in 3D scene is visible", true + ""),
+	MOVEMENT_DIRECTIONS_VISIBLE("Grid in 3D scene is visible", false + ""),
+	VISIBILITY_GRAPH_VISIBLE("Grid in 3D scene is visible", false + ""),
+	LOCAL_COORDINATE_SYSTEMS_VISIBLE("Grid in 3D scene is visible", false + ""),
+	VISUAL_ELEMENTS_VISIBLE("Grid in 3D scene is visible", true + ""),	
 	N_KNOWN("n is known to robots", true + ""),
 	FAIRNESS_COUNT("Fairness count", 10 + ""), 
 	WORKSPACE_DIR("Workspace dir", System.getProperty("user.home") + System.getProperty("file.separator") + "Documents"
 			+ System.getProperty("file.separator") + "Sycamore");
 
-	private String	name			= null;
+	private String	description			= null;
 	private String	defaultValue	= null;
 
 	/**
 	 * 
 	 */
-	ApplicationProperties(String name, String defaultValue)
+	ApplicationProperties(String description, String defaultValue)
 	{
-		this.name = name;
+		this.description = description;
 		this.defaultValue = defaultValue;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see it.diunipi.volpi.sycamore.util.SycamoreProperty#getName()
+	/**
+	 * @return the description
 	 */
-	@Override
-	public String getName()
+	public String getDescription()
 	{
-		return name;
+		return description;
 	}
 
 	/*
@@ -60,7 +70,7 @@ public enum ApplicationProperties implements SycamoreProperty
 
 		for (ApplicationProperties prop : values)
 		{
-			if (prop.getName().equals(name))
+			if (prop.name().equals(name))
 			{
 				return prop;
 			}
